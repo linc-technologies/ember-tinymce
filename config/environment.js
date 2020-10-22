848609addb8680929d86a32462f9f3d1eff5d490
Matthew Hartstonge <matt@mykro.co.nz>
2020-10-22 16:58:18 +1300

    :zap: config/environment: pins to a finer grained version of tinyMCE.

--- config/environment.js
+++ config/environment.js
@@ -5,8 +5,8 @@ module.exports = function(/* environment, appConfig */) {
     tinyMCE:{
       load: true,
       apiKey: 'no-api-key',
-      version: '5.4.1',
-      sriHash: 'sha384-oap60xwXZh7bNDdoSHVBtMsWkvS1kl6osQyFFOSDzPblo0981h44ynbbQ9RiaLj8'
+      version: '5.4.1-89',
+      sriHash: 'sha256-PdTPLT4Z8YOt3KtZGdFNluNYcdELUK2wV5Px2oCieOU= sha384-1tw/JzBN5kJwquM6AQvQlVf6eGy8lKL51d9L/FFjw1ztDlpd5rt8nETQo+LUPwDW sha512-ZgfngoOLexgu1rHVSXMbvfYLA9B2dwPSn3nHdb7EkRxodCRF5VHL3+8cpr5Ikd7fP/7/zbKaso3CWjQSJOjPOw=='
     }
   };
 };
